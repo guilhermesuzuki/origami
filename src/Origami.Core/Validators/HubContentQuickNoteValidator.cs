203daccd4beef7fcfc5ef76f6c9f6daf6bcc38cc
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-04-12 14:55:38 -0300

    Remove explicit isBlogIdRequired param in QuickNote validator
    OrigamiContentValidator is now constructed in HubContentQuickNoteValidator without explicitly passing isBlogIdRequired: false, relying on the default behavior for this parameter. This simplifies the constructor call and uses the validator's default logic.

--- src/Origami.Core/Validators/HubContentQuickNoteValidator.cs
+++ src/Origami.Core/Validators/HubContentQuickNoteValidator.cs
@@ -12,7 +12,7 @@ namespace Origami.Core.Validators
     {
         public HubContentQuickNoteValidator(Text text, IWebRootPath webRootPath, IDbContextFactory<OrigamiDbContext> dbContextFactory) : base()
         {
-            RuleFor(x => x.Entity).SetValidator(new OrigamiContentValidator(text, webRootPath, dbContextFactory, isBlogIdRequired: false));
+            RuleFor(x => x.Entity).SetValidator(new OrigamiContentValidator(text, webRootPath, dbContextFactory));
         }
     }
 }
