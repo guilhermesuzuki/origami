5b13f34cb147b33cfa98a1935cf5f8a03a60955f
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-04-28 00:12:28 -0300

    Add integration test project for Origami.UI.Admin
    Added Origami.UI.Admin.IntegrationTests project with dependencies for integration testing, including xUnit, FluentAssertions, and EF Core Sqlite. Registered the project and a "tests" folder in the solution. Implemented CustomFactory to use an in-memory Sqlite database for tests. Added a placeholder test class with a sample test method.

---
+++ tests/Origami.UI.Admin.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Origami.Core.Data;
+
+namespace Origami.UI.Admin.IntegrationTests;
+
+public class CustomFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<OrigamiDbContext>));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<OrigamiDbContext>(options =>
+            {
+                options.UseSqlite("DataSource=:memory:");
+            });
+        });
+    }
+}
---
+++ tests/Origami.UI.Admin.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Origami.UI.Admin.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
