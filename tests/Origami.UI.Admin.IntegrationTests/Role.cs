625113c36961210a4c0c63060ded3e16496be479
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-05-01 17:06:09 -0300

    Add role rights tests and name length validation checks
    Expanded OrigamiRole tests to include new rights properties and verified their persistence in the database. Added tests to ensure roles with names over 255 characters cannot be inserted or updated, and validated that original data remains unchanged on failure. Improves test coverage for role-rights associations and name length constraints.

--- tests/Origami.UI.Admin.IntegrationTests/Role.cs
+++ tests/Origami.UI.Admin.IntegrationTests/Role.cs
@@ -74,6 +74,10 @@ namespace Origami.UI.Admin.IntegrationTests
                 DateCreated = DateTime.UtcNow,
                 Name = "Another test role",
                 NanoId = Guid.NewGuid().ToString().Substring(0, 8),
+                CreateNewBlogs = true,
+                EditBlogs = true,
+                DeleteBlogs = true,
+                PurgeBlogs = true, 
             };
 
             var result = superRepository.Roles.SmartSave(anotherRole.GetContext(TestUser), checkPermission: true);
@@ -93,6 +97,24 @@ namespace Origami.UI.Admin.IntegrationTests
             cacheRole.Name.ShouldBe(anotherRole.Name);
             cacheRole.NanoId.ShouldBe(anotherRole.NanoId);
             cacheRole.IsDeleted.ShouldBe(anotherRole.IsDeleted);
+
+            using var db = superRepository.DbContextFactory.CreateDbContext();
+            var query = from a in db.RightRoles
+                        join b in db.Rights on a.RightId equals b.Id
+                        where a.RoleId == anotherRole.Id
+                        orderby b.Name
+                        select b;
+
+            var rights = query.ToList();
+            rights.Count.ShouldBe(4);
+            rights[0].ShouldNotBeNull();
+            rights[1].ShouldNotBeNull();
+            rights[2].ShouldNotBeNull();
+            rights[3].ShouldNotBeNull();
+            rights[0].Name.ShouldBe(nameof(OrigamiRole.CreateNewBlogs));
+            rights[1].Name.ShouldBe(nameof(OrigamiRole.DeleteBlogs));
+            rights[2].Name.ShouldBe(nameof(OrigamiRole.EditBlogs));
+            rights[3].Name.ShouldBe(nameof(OrigamiRole.PurgeBlogs));
         }
 
         [Fact]
@@ -160,6 +182,17 @@ namespace Origami.UI.Admin.IntegrationTests
 
             var cacheRole = superRepository.Roles.ReadFromCache().Id(anotherRole.Id);
             cacheRole.ShouldBeNull();
+
+            using var db = superRepository.DbContextFactory.CreateDbContext();
+            var query = from a in db.RightRoles
+                        join b in db.Rights on a.RightId equals b.Id
+                        where a.RoleId == anotherRole.Id
+                        orderby b.Name
+                        select b;
+
+            var rights = query.ToList();
+            rights.ShouldNotBeNull();
+            rights.Count.ShouldBe(0);
         }
 
         [Fact]
@@ -189,6 +222,54 @@ namespace Origami.UI.Admin.IntegrationTests
             var resultUpdate = superRepository.Roles.SmartSave(anotherRole.GetContext(TestUser), checkPermission: true);
             resultUpdate.ShouldNotBeNull();
             resultUpdate.Ok.ShouldBeTrue();
+
+            var dbRole = superRepository.Roles.ReadFromDatabase(anotherRole);
+            dbRole.ShouldNotBeNull();
+            dbRole.DateCreated.ShouldBe(anotherRole.DateCreated);
+            dbRole.Name.ShouldBe(anotherRole.Name);
+            dbRole.NanoId.ShouldBe(anotherRole.NanoId);
+            dbRole.IsDeleted.ShouldBe(anotherRole.IsDeleted);
+
+            var cacheRole = superRepository.Roles.ReadFromCache().Id(anotherRole.Id);
+            cacheRole.ShouldNotBeNull();
+            cacheRole.DateCreated.ShouldBe(anotherRole.DateCreated);
+            cacheRole.Name.ShouldBe(anotherRole.Name);
+            cacheRole.NanoId.ShouldBe(anotherRole.NanoId);
+            cacheRole.IsDeleted.ShouldBe(anotherRole.IsDeleted);
+        }
+
+        [Fact]
+        public void Update_WhenNameIsTooLarge_ShouldFail()
+        {
+            using var transaction = new TransactionScope();
+            using var scope = _factory.Services.CreateScope();
+            var superRepository = scope.ServiceProvider.GetService<ISuperRepository>()!;
+
+            scope.CreateTestRole(TestRole);
+            scope.CreateTestUser(TestUser, TestRole);
+
+            var anotherRole = new OrigamiRole
+            {
+                Id = Guid.NewGuid(),
+                DateCreated = DateTime.UtcNow,
+                Name = "Another test role",
+                NanoId = Guid.NewGuid().ToString().Substring(0, 8),
+            };
+
+            var result = superRepository.Roles.SmartSave(anotherRole.GetContext(TestUser), checkPermission: true);
+            result.ShouldNotBeNull();
+            result.Ok.ShouldBeTrue();
+
+            var anotherRoleUpdate = anotherRole.Clone();
+            anotherRoleUpdate.Name = "Another updated test role " + new string('a', 255);
+
+            var resultUpdate = superRepository.Roles.SmartSave(anotherRoleUpdate.GetContext(TestUser), checkPermission: true);
+            resultUpdate.ShouldNotBeNull();
+            resultUpdate.Ok.ShouldBeFalse();
+            resultUpdate.Messages.ShouldNotBeNull();
+            resultUpdate.Messages.Count.ShouldBe(1);
+            resultUpdate.Messages[0].MessageType.ShouldBe(ResultMessage.MessageTypes.Error);
+            resultUpdate.Messages[0].Message.ShouldBe("Name cannot exceed 255 characters");
 
             var dbRole = superRepository.Roles.ReadFromDatabase(anotherRole);
             dbRole.ShouldNotBeNull();
