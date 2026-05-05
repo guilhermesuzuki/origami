ac7183a0647f639001d3621b10e4b234503d8345
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-05-05 07:47:52 -0300

    Add purge options and lifecycle test for hub content
    Added purge-related boolean flags to CustomClassFixture config. Introduced a test covering create, delete, and purge operations for hub content, verifying related categories, tags, and history in both DB and cache.

--- tests/Origami.UI.Admin.IntegrationTests/HubContentTests.cs
+++ tests/Origami.UI.Admin.IntegrationTests/HubContentTests.cs
@@ -277,6 +277,191 @@ namespace Origami.UI.Admin.IntegrationTests
         }
 
         [Fact]
+        public void Purge_WhenEntityIsValid_ShouldPersistRecord()
+        {
+            using var factory = new CustomWebApplicationFactory();
+            using var transaction = new TransactionScope();
+            using var scope = factory.Services.CreateScope();
+            using var db = scope.ServiceProvider.GetRequiredService<IDbContextFactory<OrigamiDbContext>>().CreateDbContext();
+            var superRepository = scope.ServiceProvider.GetRequiredService<ISuperRepository>();
+
+            scope.CreateTestBlog(TestBlog, TestRole, TestUser);
+            scope.CreateTestCategory(TestCategory, TestUser);
+
+            var t2 = Activator.CreateInstance<T2>()!;
+            if (t2 is HubContentPage or HubContentPost or HubContentVideo)
+            {
+                t2.Entity.BlogId = TestBlog.Id;
+            }
+            if (t2 is HubContentPost or HubContentVideo)
+            {
+                t2.Categories.Add(new() { CategoryId = TestCategory.Id, ContentId = t2.Id });
+                t2.Tags.Add(new() { ContentId = t2.Id, Tag = "Test tag", Slug = "Test tag".GetSlug() });
+            }
+
+            t2.Entity.AuthorId = TestUser.Id;
+            t2.Entity.Content = "<p>Test content</p>";
+            t2.Entity.Description = "Test description";
+            t2.Entity.LanguageWrittenOn = "en-US";
+            t2.Entity.Slug = "Test title".GetSlug();
+            t2.Entity.Title = "Test title";
+
+            var hubRepository = scope.ServiceProvider.GetRequiredService<IHubContentRepository<T2>>();
+            var hub = hubRepository.Save(t2, TestUser);
+
+            hub.ShouldNotBeNull();
+            hub.Ok.ShouldBeTrue();
+
+            var dbEntity = db.Set<T1>().Find(t2.Entity.Id);
+            dbEntity.ShouldNotBeNull();
+            dbEntity.Content.ShouldBe(t2.Entity.Content);
+            dbEntity.Description.ShouldBe(t2.Entity.Description);
+            dbEntity.LanguageWrittenOn.ShouldBe(t2.Entity.LanguageWrittenOn);
+            dbEntity.Slug.ShouldBe(t2.Entity.Slug);
+            dbEntity.Title.ShouldBe(t2.Entity.Title);
+            dbEntity.IsDeleted.ShouldBeFalse();
+            dbEntity.IsDeleted.ShouldBe(t2.Entity.IsDeleted);
+
+            var cacheHubContent = hubRepository.Get(t2);
+            cacheHubContent.Entity.Content.ShouldBe(t2.Entity.Content);
+            cacheHubContent.Entity.Description.ShouldBe(t2.Entity.Description);
+            cacheHubContent.Entity.LanguageWrittenOn.ShouldBe(t2.Entity.LanguageWrittenOn);
+            cacheHubContent.Entity.Slug.ShouldBe(t2.Entity.Slug);
+            cacheHubContent.Entity.Title.ShouldBe(t2.Entity.Title);
+            cacheHubContent.Entity.IsDeleted.ShouldBeFalse();
+            cacheHubContent.Entity.IsDeleted.ShouldBe(t2.Entity.IsDeleted);
+            cacheHubContent.Categories.Count.ShouldBe(t2.Categories.Count);
+            cacheHubContent.Tags.Count.ShouldBe(t2.Tags.Count);
+
+            if (t2 is HubContentPage)
+            {
+                cacheHubContent.Categories.Count.ShouldBe(0);
+                cacheHubContent.Tags.Count.ShouldBe(0);
+            }
+
+            if (t2 is HubContentPost or HubContentVideo)
+            {
+                cacheHubContent.Categories.Count.ShouldBe(1);
+                cacheHubContent.Categories[0].CategoryId.ShouldBe(TestCategory.Id);
+                cacheHubContent.Categories[0].ContentId.ShouldBe(t2.Entity.Id);
+                cacheHubContent.Categories[0].Version.ShouldBe(t2.Categories[0].Version);
+
+                var dbCategories = db.Set<OrigamiContentCategory>().Where(x => x.ContentId == t2.Entity.Id).ToList();
+                dbCategories.Count.ShouldBe(1);
+                dbCategories[0].CategoryId.ShouldBe(TestCategory.Id);
+                dbCategories[0].ContentId.ShouldBe(t2.Entity.Id);
+                dbCategories[0].Version.ShouldBe(t2.Categories[0].Version);
+
+                cacheHubContent.Tags.Count.ShouldBe(1);
+                cacheHubContent.Tags[0].ContentId.ShouldBe(t2.Entity.Id);
+                cacheHubContent.Tags[0].Tag.ShouldBe(t2.Tags[0].Tag);
+                cacheHubContent.Tags[0].Slug.ShouldBe(t2.Tags[0].Slug);
+                cacheHubContent.Tags[0].Version.ShouldBe(t2.Tags[0].Version);
+
+                var dbTags = db.Set<OrigamiContentTag>().Where(x => x.ContentId == t2.Entity.Id).ToList();
+                dbTags.Count.ShouldBe(1);
+                dbTags[0].ContentId.ShouldBe(t2.Entity.Id);
+                dbTags[0].Tag.ShouldBe(t2.Tags[0].Tag);
+                dbTags[0].Slug.ShouldBe(t2.Tags[0].Slug);
+                dbTags[0].Version.ShouldBe(t2.Tags[0].Version);
+            }
+
+            var dbHubHistories = db.Set<OrigamiContentHistory>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).ToList();
+            dbHubHistories.Count.ShouldBe(1);
+            dbHubHistories[0].Description.ShouldBe("Content created");
+
+            var deleteHub = hubRepository.Delete(t2, TestUser);
+
+            deleteHub.ShouldNotBeNull();
+            deleteHub.Ok.ShouldBeTrue();
+
+            dbEntity = db.Set<T1>().AsNoTracking().Id(t2.Entity.Id);
+            dbEntity.ShouldNotBeNull();
+            dbEntity.Content.ShouldBe(t2.Entity.Content);
+            dbEntity.Description.ShouldBe(t2.Entity.Description);
+            dbEntity.LanguageWrittenOn.ShouldBe(t2.Entity.LanguageWrittenOn);
+            dbEntity.Slug.ShouldBe(t2.Entity.Slug);
+            dbEntity.Title.ShouldBe(t2.Entity.Title);
+            dbEntity.IsDeleted.ShouldBeTrue();
+            dbEntity.IsDeleted.ShouldBe(t2.Entity.IsDeleted);
+
+            cacheHubContent = hubRepository.Get(t2);
+            cacheHubContent.Entity.Content.ShouldBe(t2.Entity.Content);
+            cacheHubContent.Entity.Description.ShouldBe(t2.Entity.Description);
+            cacheHubContent.Entity.LanguageWrittenOn.ShouldBe(t2.Entity.LanguageWrittenOn);
+            cacheHubContent.Entity.Slug.ShouldBe(t2.Entity.Slug);
+            cacheHubContent.Entity.Title.ShouldBe(t2.Entity.Title);
+            cacheHubContent.Entity.IsDeleted.ShouldBeTrue();
+            cacheHubContent.Entity.IsDeleted.ShouldBe(t2.Entity.IsDeleted);
+            cacheHubContent.Categories.Count.ShouldBe(t2.Categories.Count);
+            cacheHubContent.Tags.Count.ShouldBe(t2.Tags.Count);
+
+            if (t2 is HubContentPost or HubContentVideo)
+            {
+                cacheHubContent.Categories.Count.ShouldBe(1);
+                cacheHubContent.Categories[0].CategoryId.ShouldBe(TestCategory.Id);
+                cacheHubContent.Categories[0].ContentId.ShouldBe(t2.Entity.Id);
+                cacheHubContent.Categories[0].Version.ShouldBe(t2.Categories[0].Version);
+
+                var dbCategories = db.Set<OrigamiContentCategory>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).ToList();
+                dbCategories.Count.ShouldBe(1);
+                dbCategories[0].CategoryId.ShouldBe(TestCategory.Id);
+                dbCategories[0].ContentId.ShouldBe(t2.Entity.Id);
+                dbCategories[0].Version.ShouldBe(t2.Categories[0].Version);
+
+                cacheHubContent.Tags = cacheHubContent.Tags
+                    .OrderBy(x => x.ContentId)
+                    .ThenBy(x => x.Tag)
+                    .ToList();
+
+                cacheHubContent.Tags.Count.ShouldBe(1);
+                cacheHubContent.Tags[0].ContentId.ShouldBe(t2.Entity.Id);
+                cacheHubContent.Tags[0].Tag.ShouldBe(t2.Tags[0].Tag);
+                cacheHubContent.Tags[0].Slug.ShouldBe(t2.Tags[0].Slug);
+                cacheHubContent.Tags[0].Version.ShouldBe(t2.Tags[0].Version);
+
+                var dbTags = db.Set<OrigamiContentTag>().AsNoTracking()
+                    .Where(x => x.ContentId == t2.Entity.Id)
+                    .OrderBy(x => x.ContentId)
+                    .ThenBy(x => x.Tag)
+                    .ToList();
+
+                dbTags.Count.ShouldBe(1);
+                dbTags[0].ContentId.ShouldBe(t2.Entity.Id);
+                dbTags[0].Tag.ShouldBe(t2.Tags[0].Tag);
+                dbTags[0].Slug.ShouldBe(t2.Tags[0].Slug);
+                dbTags[0].Version.ShouldBe(t2.Tags[0].Version);
+            }
+
+            dbHubHistories = db.Set<OrigamiContentHistory>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).OrderBy(x => x.DateCreated).ToList();
+            dbHubHistories.Count.ShouldBe(2);
+            dbHubHistories[0].Description.ShouldBe("Content created");
+            dbHubHistories[1].Description.ShouldBe("Content deleted");
+
+            var purgeHub = hubRepository.Purge(t2, TestUser);
+            purgeHub.ShouldNotBeNull();
+            purgeHub.Ok.ShouldBeTrue();
+
+            dbEntity = db.Set<T1>().AsNoTracking().Id(t2.Entity.Id);
+            dbEntity.ShouldBeNull();
+
+            var dbPurgedCategories = db.Set<OrigamiContentCategory>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).ToList();
+            dbPurgedCategories.ShouldBeEmpty();
+
+            var dbPurgedTags = db.Set<OrigamiContentTag>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).ToList();
+            dbPurgedTags.ShouldBeEmpty();
+
+            dbHubHistories = db.Set<OrigamiContentHistory>().AsNoTracking().Where(x => x.ContentId == t2.Entity.Id).OrderBy(x => x.DateCreated).ToList();
+            dbHubHistories.ShouldBeEmpty();
+
+            cacheHubContent = hubRepository.Get(t2);
+            cacheHubContent.Entity.Id.ShouldNotBe(t2.Entity.Id);
+            cacheHubContent.Parent.ShouldBeNull();
+            cacheHubContent.Children.ShouldBeEmpty();
+            cacheHubContent.Categories.ShouldBeEmpty(); 
+            cacheHubContent.Tags.ShouldBeEmpty();
+        }
+        [Fact]
         public void Update_WhenEntityIsValid_ShouldPersistRecord()
         {
             using var factory = new CustomWebApplicationFactory();
