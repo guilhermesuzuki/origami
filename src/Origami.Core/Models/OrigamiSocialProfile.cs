062c275b27bc16bf82bbc76e581de02b3fee3bfc
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-07-27 11:22:35 -0300

    Refactor entity editing to use query string for NanoId
    Refactored Blazor admin and content grids to handle entity editing by updating the "nanoid" query string via JS interop instead of URL navigation. Made OnEdit and EntityCreated async, updated entity selection logic, simplified @page directives, removed obsolete code, and improved date display with Humanize().

--- src/Origami.Core/Models/OrigamiSocialProfile.cs
+++ src/Origami.Core/Models/OrigamiSocialProfile.cs
@@ -6,8 +6,8 @@ namespace Origami.Core.Models
 {
     [Table("oi_SocialProfiles")]
     public class OrigamiSocialProfile :
+        BaseModel,
         IChanged,
-        IId,
         IName,
         IEmail,
         IVersion,
