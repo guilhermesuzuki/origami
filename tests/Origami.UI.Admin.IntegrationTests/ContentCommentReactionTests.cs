b72c6d9253a2eecc1564f74ae913deafd68ad7f4
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-07-30 13:41:55 -0300

    Add test for purging content comment reactions
    Added Purge_WhenEntityIsValid_ShouldPersistRecord test to ContentCommentReactionTests. This test ensures that creating and purging a content comment reaction correctly persists related event records in the database, including SocialProfileReactsToCommentEvent and SocialProfileCancelsReactionToCommentEvent.

--- tests/Origami.UI.Admin.IntegrationTests/ContentCommentReactionTests.cs
+++ tests/Origami.UI.Admin.IntegrationTests/ContentCommentReactionTests.cs
@@ -59,5 +59,62 @@ namespace Origami.UI.Admin.IntegrationTests
                          select a;
             var dbEvent = query2.Single();
         }
+
+        [Fact]
+        public void Purge_WhenEntityIsValid_ShouldPersistRecord()
+        {
+            using var factory = new CustomWebApplicationFactory();
+            using var transaction = new TransactionScope();
+            using var scope = factory.Services.CreateScope();
+            using var db = scope.ServiceProvider.GetRequiredService<IDbContextFactory<OrigamiDbContext>>().CreateDbContext();
+            var superRepository = scope.ServiceProvider.GetRequiredService<ISuperRepository>();
+
+            _contentCommentTests.Insert_WhenEntityIsValid_ShouldPersistRecord(false);
+
+            var reaction = new OrigamiContentCommentReaction
+            {
+                CommentId = Comment.Id,
+                DateCreated = DateTime.UtcNow,
+                IsBot = false,
+                IsMobileDevice = false,
+                SocialProfileId = TestFacebookId,
+                Reaction = "❤️",
+            };
+
+            superRepository.ContentCommentReactions.SmartCreate(new(TestFacebookProfile, DateTime.UtcNow, reaction));
+
+            // private scope
+            {
+                var query = from a in db.ContentCommentReactions.AsNoTracking() where a.Id == reaction.Id select a;
+                var dbReaction = query.Single();
+                dbReaction.CommentId.ShouldBe(reaction.CommentId);
+                dbReaction.DateCreated.ShouldBe(reaction.DateCreated);
+                dbReaction.IsBot.ShouldBe(reaction.IsBot);
+                dbReaction.IsMobileDevice.ShouldBe(reaction.IsMobileDevice);
+                dbReaction.SocialProfileId.ShouldBe(reaction.SocialProfileId);
+                dbReaction.Reaction.ShouldBe(reaction.Reaction);
+            }
+
+            //private scope
+            {
+                var query = from a in db.Events.AsNoTracking().OfType<SocialProfileReactsToCommentEvent>()
+                             where a.ReactionId == reaction.Id
+                             where a.SocialProfileId == reaction.SocialProfileId
+                             select a;
+                var dbEvent = query.Single();
+            }
+            
+
+            superRepository.ContentCommentReactions.SmartPurge(new(TestFacebookProfile, DateTime.UtcNow, reaction));
+
+            //private scope
+            {
+                var query = from a in db.Events.AsNoTracking().OfType<SocialProfileCancelsReactionToCommentEvent>()
+                            where a.ReactionId == reaction.Id
+                            where a.SocialProfileId == reaction.SocialProfileId
+                            select a;
+                var dbEvent = query.Single();
+            }
+        }
     }
 }
