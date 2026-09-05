d034b5f2e6beaa40a5aad2f0f88627eb4a18df50
Guilherme Ishikawa Suzuki <suzuki.gui@gmail.com>
2026-09-05 00:54:44 -0300

    Refactor View<T>: simplify logic, remove try/finally
    Refactored the View<T> method to eliminate the try/finally block and streamline logic. Now, the method first searches for a physical page by virtualPath; if not found, it creates and saves a new one. Assignment of UserId or SocialProfileId is handled outside the removed block using standard if/else if statements. After saving, the in-memory view count is incremented if successful. The method returns the result of the save operation.

--- src/Origami.Core/Data/PhysicalPageRepository.cs
+++ src/Origami.Core/Data/PhysicalPageRepository.cs
@@ -29,30 +29,27 @@ namespace Origami.Core.Data
 
         public Result<OrigamiPhysicalPageView> View<T>(string virtualPath, OrigamiPhysicalPageView view, T whoIsResponsible)
         {
-            try
+            var physicalPage = this.ReadFromCache().FirstOrDefault(x => x.Path == virtualPath);
+            if (physicalPage == null)
             {
-                var physicalPage = this.ReadFromCache().FirstOrDefault(x => x.Path == virtualPath);
-                if (physicalPage == null)
-                {
-                    physicalPage = new() { Path = virtualPath, DateCreated = DateTime.UtcNow };
-                    var createResult = this.SmartSave(physicalPage.GetContext(), false);
-                    if (createResult.Ok == false) return new Result<OrigamiPhysicalPageView>(view).Pull(createResult);
-                }
-
-                view.PhysicalPageId = physicalPage.Id;
-
-                if (whoIsResponsible is OrigamiUser user && user.Id != Guid.Empty)
-                {
-                    view.UserId = user.Id;
-                }
-                else if (whoIsResponsible is OrigamiSocialProfile socialProfile && socialProfile.Id != Guid.Empty)
-                {
-                    view.SocialProfileId = socialProfile.Id;
-                }
-
-                return this._physicalPageViewRepository.SmartSave(view.GetContext(), false);
+                physicalPage = new() { Path = virtualPath, DateCreated = DateTime.UtcNow };
+                var createResult = this.SmartSave(physicalPage.GetContext(), false);
+                if (createResult.Ok == false) return new Result<OrigamiPhysicalPageView>(view).Pull(createResult);
             }
-            finally
+
+            view.PhysicalPageId = physicalPage.Id;
+
+            if (whoIsResponsible is OrigamiUser user && user.Id != Guid.Empty)
+            {
+                view.UserId = user.Id;
+            }
+            else if (whoIsResponsible is OrigamiSocialProfile socialProfile && socialProfile.Id != Guid.Empty)
+            {
+                view.SocialProfileId = socialProfile.Id;
+            }
+
+            var hub = this._physicalPageViewRepository.SmartSave(view.GetContext(), false);
+            if (hub.Ok)
             {
                 lock (OrigamiConstants.SyncRoot)
                 {
@@ -60,6 +57,7 @@ namespace Origami.Core.Data
                     this.MemoryCache.Set(virtualPath, ++count);
                 }
             }
+            return hub;
         }
 
         public long Views(string virtualPath)
